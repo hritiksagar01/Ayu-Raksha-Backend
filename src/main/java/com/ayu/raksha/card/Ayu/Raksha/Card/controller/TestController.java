0a8305a26161988eff5ca79d75fefd62ee796e2d
hritiksagar01 <143049203hritiksagar01@users.noreply.github.com>
2025-12-07 14:08:01 +0530

    testcontroler

---
+++ src/main/java/com/ayu/raksha/card/Ayu/Raksha/Card/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.ayu.raksha.card.Ayu.Raksha.Card.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    public String test() {
+        return "Test successful!";
+    }
+}
